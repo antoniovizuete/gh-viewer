244534412502af0d2e595841350014f9faef3db9
Antonio Vizuete <antoniovizuete@gmail.com>
2022-03-21 23:28:04 +0100

    Retrive and show the issues and pull requests.

--- src/pages/RepoIssuesPage.tsx
+++ src/pages/RepoIssuesPage.tsx
@@ -1,9 +1,54 @@
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
+
+type Issue = {
+  id: number,
+  title: string,
+  user: {
+    login: string,
+  }
+  created_at: string,
+  comments: number,
+  labels: {
+    id: number,
+    name: string,
+    description: string,
+    color: string,
+  }[],
+  pull_request: Record<string, unknown>
+}
 
 export default function RepoIssuesPage() {
   const { username, repo } = useParams()
- 
+  const [issues, setIssues] = useState<Issue[]>([])
+  const [loading, setLoading] = useState(true)
+  
+  useEffect(() => {
+    fetch(`https://api.github.com/repos/${username}/${repo}/issues?state=open`)
+      .then(res => res.json())
+      .then(setIssues)
+      .finally(() => setLoading(false))
+  } ,[])
+
+  if (loading) {
+    return <h1>Loading...</h1>
+  }
+  if (issues.length === 0) {
+    return <h1>No issues</h1>
+  }
   return (
-   <div>{`${username} ${repo}`}</div>
+   issues.map(issue => (
+     <ul key={issue.id}>
+       <li>{issue.title}</li>
+       <li>{issue.user.login}</li>
+       <li>{issue.created_at}</li>
+       <li>{issue.comments}</li>
+       <ul>{issue.labels.map(label => (
+        <li key={label.id}>{label.name}</li>
+        ))}</ul>
+        {issue.pull_request && "Pull Request"}
+        {!issue.pull_request && "Issue"}
+     </ul>
+   ))
   )
 }
