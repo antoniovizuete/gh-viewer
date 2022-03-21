9fe36d44aabd9b1a374c78e84db661132d190c31
Antonio Vizuete <antoniovizuete@gmail.com>
2022-03-21 15:55:56 +0100

    Add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,11 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import Logo from './components/Logo'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className='min-h-screen flex justify-center items-center'>
+      <Logo className='fill-blue-400 h-48'/>
+      <h1 className='text-6xl font-semibold text-blue-500'>gh-viewer</h1>
     </div>
   )
 }
