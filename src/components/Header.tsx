4886a68ec851aa3a59de54e5e51f83ef4a08d483
Antonio Vizuete <antoniovizuete@gmail.com>
2022-03-22 22:42:55 +0100

    Restyle Header component.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,8 +5,8 @@ export default function Header() {
   return (
     <header className='fixed top-0 left-0 w-full h-24 pt-4 bg-slate-100 z-50 backdrop-blur-sm bg-slate-100/80'>
       <Link to="/" className='w-fit mx-auto flex justify-center items-center'>
-        <Logo className='fill-blue-500 h-16' />
-        <h1 className='text-4xl font-semibold text-blue-500'>gh-viewer</h1>
+        <Logo className='fill-slate-700 h-12 sm:h-16' />
+        <h1 className='text-4xl font-semibold text-slate-700'>gh-viewer</h1>
       </Link>
     </header>
   )
